dd54b1beeeb7869348b8b088e163355e36cb7c65
Aroy <royaritra1990rocks@gmail.com>
2023-03-11 05:10:32 +0530

    Solidity Update

---
+++ contracts/Election.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <=0.8.19;
+
+contract Election {
+    // Read/write candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor () public{
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+}
